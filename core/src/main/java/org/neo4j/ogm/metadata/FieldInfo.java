6d54371bd675d20c30720e7444474c6039b56a7b
Michael J. Simons <michael.simons@neo4j.com>
2018-11-10 11:26:49 +0100

    Use the type system for native properties for OGM to database.

--- core/src/main/java/org/neo4j/ogm/metadata/FieldInfo.java
+++ core/src/main/java/org/neo4j/ogm/metadata/FieldInfo.java
@@ -72,6 +72,7 @@ public class FieldInfo {
     private final String typeParameterDescriptor;
     private final ObjectAnnotations annotations;
     private final boolean isArray;
+    private final boolean isSupportedNativeType;
     private final ClassInfo containingClassInfo;
     private final Field field;
     private final Class<?> fieldType;
@@ -92,7 +93,7 @@ public class FieldInfo {
      *                                if that's not appropriate
      * @param annotations             The {@link ObjectAnnotations} applied to the field
      */
-    public FieldInfo(ClassInfo classInfo, Field field, String typeParameterDescriptor, ObjectAnnotations annotations) {
+    public FieldInfo(ClassInfo classInfo, Field field, String typeParameterDescriptor, ObjectAnnotations annotations, boolean isSupportedNativeType) {
         this.containingClassInfo = classInfo;
         this.field = field;
         this.fieldType = field.getType();
@@ -101,6 +102,7 @@ public class FieldInfo {
         this.descriptor = field.getType().getTypeName();
         this.typeParameterDescriptor = typeParameterDescriptor;
         this.annotations = annotations;
+        this.isSupportedNativeType = isSupportedNativeType;
         if (!this.annotations.isEmpty()) {
             Object converter = getAnnotations().getConverter(this.fieldType);
             if (converter instanceof AttributeConverter) {
@@ -184,6 +186,7 @@ public class FieldInfo {
     public boolean persistableAsProperty() {
 
         return PRIMITIVES.contains(descriptor)
+            || isSupportedNativeType
             || (AUTOBOXERS.contains(descriptor) && typeParameterDescriptor == null)
             || (typeParameterDescriptor != null && AUTOBOXERS.contains(typeParameterDescriptor))
             || propertyConverter != null
